35ebe4d5bd156ee5a6a4b8303f591810557a0d69
Florian Knip <florian@knip-builds.de>
2023-10-24 23:08:44 +0200

    fix: circle around css

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ export interface Props {
 }
 
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
-  const { title, pubDatetime, description } = frontmatter;
+  const { title, pubDatetime } = frontmatter;
 
   const headerProps = {
     style: { viewTransitionName: slugifyStr(title) },
